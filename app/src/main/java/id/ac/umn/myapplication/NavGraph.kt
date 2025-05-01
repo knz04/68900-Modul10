48cd5cc410fc61e21c51e6ad2d0ac19495058f9b
hawyeet <53439203+knz04@users.noreply.github.com>
2025-05-01 12:03:44 +0700

    Bagian 1

---
+++ app/src/main/java/id/ac/umn/myapplication/NavGraph.kt
@@ -0,0 +1,16 @@
+package id.ac.umn.myapplication
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun AppNavGraph(navController: NavHostController, authViewModel: AuthViewModel = viewModel()) {
+    NavHost(navController, startDestination = if (authViewModel.getCurrentUser() != null) "home" else "login") {
+        composable("login") { LoginScreen(navController, authViewModel) }
+        composable("signup") { SignupScreen(navController, authViewModel) }
+        composable("home") { HomeScreen(navController, authViewModel) }
+    }
+}
