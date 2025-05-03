2e31af5795718d0e01f415d8b02594ac0a590e65
hawyeet <53439203+knz04@users.noreply.github.com>
2025-05-03 12:28:45 +0700

    Bagian 2

--- app/src/main/java/id/ac/umn/myapplication/AuthViewModel.kt
+++ app/src/main/java/id/ac/umn/myapplication/AuthViewModel.kt
@@ -3,6 +3,7 @@ package id.ac.umn.myapplication
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class AuthViewModel : ViewModel() {
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
@@ -35,5 +36,17 @@ class AuthViewModel : ViewModel() {
 
     fun signOut() {
         auth.signOut()
+    }
+
+    fun signInWithGoogle(idToken: String, onResult: (Boolean, String?) -> Unit) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    onResult(true, null)
+                } else {
+                    onResult(false, task.exception?.message)
+                }
+            }
     }
 }
--- app/src/main/java/id/ac/umn/myapplication/LoginScreen.kt
+++ app/src/main/java/id/ac/umn/myapplication/LoginScreen.kt
@@ -1,5 +1,8 @@
 package id.ac.umn.myapplication
 
+import android.app.Activity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -21,8 +24,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 @Composable
 fun LoginScreen(navController: NavController, authViewModel: AuthViewModel) {
@@ -31,6 +37,30 @@ fun LoginScreen(navController: NavController, authViewModel: AuthViewModel) {
 
     val snackbarHostState = remember { SnackbarHostState() }
     var snackbarMessage by remember { mutableStateOf<String?>(null) }
+    val context = LocalContext.current
+    val activity = context as Activity
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+        if(task.isSuccessful) {
+            val idToken = task.result?.idToken
+            if (idToken != null) {
+                authViewModel.signInWithGoogle(idToken) { success, errorMessage ->
+                    if(success) {
+                        navController.navigate("home") {
+                            popUpTo("login") { inclusive = true } }
+                    } else {
+                        snackbarMessage = errorMessage ?: "Unknown error"
+                    }
+                } 
+            } else {
+                snackbarMessage = "Google SIgn-In failed."
+            }
+        } else {
+            snackbarMessage = "Google Sign-In cancelled."
+        }
+    }
 
     Scaffold(
         snackbarHost = { SnackbarHost(snackbarHostState) }
@@ -67,6 +97,16 @@ fun LoginScreen(navController: NavController, authViewModel: AuthViewModel) {
                 }
             }) {
                 Text("Login")
+            }
+            Button(onClick = {
+                var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(activity.getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+                val googleSignInClient = GoogleSignIn.getClient(activity, gso)
+                launcher.launch(googleSignInClient.signInIntent)
+            }) {
+                Text("Continue with Google")
             }
             Spacer(modifier = Modifier.height(8.dp))
             TextButton(onClick = { navController.navigate("signup") }) {
